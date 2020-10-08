4c665e75305f2afda4e82b98d0acae9638ddf193
Victor <contact@victordarcel.com>
2020-10-08 17:14:17 +0200

    add Google Font Roboto in gasby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,15 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Roboto\:500`
+        ],
+        display: 'swap'
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
