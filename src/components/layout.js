2afa43bcf39f391075ed9912a7928439b9309054
Victor <contact@victordarcel.com>
2020-10-13 11:09:10 +0200

    fix globalStyle in layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-import GlobalStyle from "./GlobalStyle"
+import GlobalStyle from "./globalStyle"
 
 import Navbar from "./NavbarTransparency"
 import Footer from "./Footer"
