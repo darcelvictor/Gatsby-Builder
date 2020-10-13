ee61b8716e24282ba7363681a41d2c0465f8f683
Victor <contact@victordarcel.com>
2020-10-13 11:31:38 +0200

    fix font error

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,21 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Roboto`,
+            variants: [`500`],
+          },
+          {
+            family: `Montserrat`,
+            variants: [`400`],
+          },
+        ],
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -47,21 +62,7 @@ module.exports = {
     //     display: 'swap'
     //   }
     // },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [
-          {
-            family: `Roboto`,
-            subsets: [`500`],
-          },
-          {
-            family: `Montserrat`,
-            variants: [`400`]
-          },
-        ],
-      },
-    },
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
