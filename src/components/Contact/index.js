bf892c89008694a1a9c36c8ac4d1c47760890d15
Victor <contact@victordarcel.com>
2020-10-13 15:56:21 +0200

    fix contact redirection 2

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ContactForm = () => (
-<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/thank-you/">
+<form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field" action="/contact/thank-you/">
   {/* You still need to add the hidden input with the form name to your JSX form */}
     <input type="hidden" name="form-name" value="contact" />
     <input type="text" name="name" />
