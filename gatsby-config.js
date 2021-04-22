605cdff24ffbfa73d49ff9d8428fe4f955181222
Victor <darcel.v@gmail.com>
2021-04-22 15:54:26 +0200

    delete unused extesions from gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,21 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       {
-    //         family: `Roboto`,
-    //         variants: [`500`],
-    //       },
-    //       {
-    //         family: `Montserrat`,
-    //         variants: [`400`],
-    //       },
-    //     ],
-    //   },
-    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -45,24 +30,13 @@ module.exports = {
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-fontawesome-css`,
     {
-      resolve: 'gatsby-plugin-react-svg',
+      resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
-          include: /\.inline\.svg$/
-        }
-      }
+          include: /\.inline\.svg$/,
+        },
+      },
     },
-    // {
-    //   resolve: `gatsby-plugin-google-fonts`,
-    //   options: {
-    //     fonts: [
-    //       `Roboto\:500`,
-    //       `Montserrat\:400`
-    //     ],
-    //     display: 'swap'
-    //   }
-    // },
-    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
